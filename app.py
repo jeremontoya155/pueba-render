d379e0ef9bae6f29c8819b708251e9cb15064c0f
Jeremias <jeremontoya155@gmail.com>
2024-02-06 15:59:07 -0300

    Cambios

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if 'excelFile' not in request.files:
+            return 'No se ha seleccionado ningún archivo'
+        
+        file = request.files['excelFile']
+        
+        if file.filename == '':
+            return 'No se ha seleccionado ningún archivo'
+        
+        if file:
+            try:
+                df = pd.read_excel(file)
+                table_html = df.to_html(classes='table table-striped')
+                return render_template('index.html', table=table_html)
+            except Exception as e:
+                return f'Ocurrió un error al procesar el archivo: {e}'
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
-   (app.run(host='0.0.0.0'))
+    app.run(debug=True)
